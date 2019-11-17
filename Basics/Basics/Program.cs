320ddd66eb36d457970fba85d20af808a23f081d
Stefan Fanaru <fanaru.stefan@gmail.com>
2019-11-17 14:18:10 +0200

    Added pb.1

--- Basics/Basics/Program.cs
+++ Basics/Basics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace Basics
 {
@@ -6,7 +7,28 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pb1();
         }
+
+        /// <summary>
+        /// 1. Write a method that reads from the console three numbers of type int and prints their sum.
+        /// </summary>
+        static void Pb1()
+        {
+            Console.WriteLine("Please enter a number: ");
+            string number1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter another number: ");
+            string number2 = Console.ReadLine();
+
+            Console.WriteLine("Please enter one more number: ");
+            string number3 = Console.ReadLine();
+
+            Console.WriteLine("The numbers you entered are: " +number1 + ", " + number2 + ", " + number3+".");
+            Console.WriteLine($"The numbers you entered are: {number1}, {number2}, {number3}.");
+
+        }
+
+        
     }
 }
