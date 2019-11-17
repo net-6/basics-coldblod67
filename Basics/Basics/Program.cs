6195b7f66812d1ebf688cd8e2f9bcf2cbcc834f6
coldblod67 <fanru.stefan@gmail.com>
2019-11-17 14:56:56 +0200

    update

---
+++ Basics/Basics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pb1();
+        }
+
+        /// <summary>
+        /// 1. Write a method that reads from the console three numbers of type int and prints their sum.
+        /// </summary>
+        static void Pb1()
+        {
+            Console.WriteLine("Please enter a number: ");
+            string number1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter another number: ");
+            string number2 = Console.ReadLine();
+
+            Console.WriteLine("Please enter one more number: ");
+            string number3 = Console.ReadLine();
+
+            Console.WriteLine($"The numbers you entered are: {number1}, {number2}, {number3}.");
+
+        }
+
+        
+    }
+}
